5d1356ba847cc2493dd59434c71665bbefc2738a
Xiaolong Mo <moxiaolong@tuliu.com>
2022-06-17 17:31:01 +0800

    update

--- lisp/init-end.el
+++ lisp/init-end.el
@@ -1,6 +1,9 @@
 ;; 最后加载的文件
-
+;; 全局开启 company 
 (global-company-mode)
+
+;; pyim 初始化 dbcaches 如果这里不初始化，而又没有使用过pyim的话，就无法使用 pyim-forward-word和 pyim-backward-word
+(pyim-process-init-dcaches)
 
 ;; 隐藏modeline
 ;(setq-default mode-line-format nil)
