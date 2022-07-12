224f2eb2197b99d42eabbff9abf5662a8affd73a
Xiaolong Mo <moxiaolong@tuliu.com>
2022-07-12 20:43:28 +0800

    update

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -327,4 +327,16 @@
          ("C-M-<" . mc/skip-to-previous-like-this) ;; 跳过当前单词并跳到上一个单词，和上面在同一个流程里。
          ("C-c C->" . mc/mark-all-symbols-like-this))) ;; 直接多选本 buffer 所有这个单词
 
+;; 终端
+(use-package vterm
+  ;; https://github.com/akermu/emacs-libvterm
+  ;; 请务必参照项目 README 作配置，以下不是我的完整配置。
+  ;; 比如，如果你要和 shell 双向互动（对，它可以双向互动），
+  ;; 那么 shell 需要做一点配置以解析 vterm 传递过来的信号
+  :config
+  (setq vterm-kill-buffer-on-exit t)) ;; shell 退出时 kill 掉这个 buffer
+;; 使用 M-x vterm 新建一个 terminal
+;; 在 terminal 中使用 C-c C-t 进入「选择」模式（类似 Tmux 里的 C-b [ ）
+
+
 (provide 'init-packages)
