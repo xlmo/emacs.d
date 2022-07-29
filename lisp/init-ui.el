fb227ebd1f9afa2a4a134cc9e0b2e3377e6ea335
Xiaolong Mo <moxiaolong@tuliu.com>
2022-07-29 17:48:29 +0800

    update

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -79,22 +79,22 @@
 
 ;; 参考: https://github.com/DogLooksGood/dogEmacs/blob/master/elisp/init-font.el
 ;; 缺省字体（英文，如显示代码）。
-(setq +font-family "Fira Code Retina")
-(setq +modeline-font-family "Fira Code Retina")
-;; 其它均使用 Sarasa Mono SC 字体。
-(setq +fixed-pitch-family "Sarasa Mono SC")
-(setq +variable-pitch-family "Sarasa Mono SC")
-(setq +font-unicode-family "Sarasa Mono SC")
-(setq +font-size 10)
+;; (setq +font-family "Fira Code Retina")
+;; (setq +modeline-font-family "Fira Code Retina")
+;; ;; 其它均使用 Sarasa Mono SC 字体。
+;; (setq +fixed-pitch-family "Sarasa Mono SC")
+;; (setq +variable-pitch-family "Sarasa Mono SC")
+;; (setq +font-unicode-family "Sarasa Mono SC")
+;; (setq +font-size 10)
 
 
-(add-hook 'after-make-frame-functions
-          ( lambda (f)
-            (+load-face-font f)
-            (+load-ext-font)
-            (+load-emoji-font)))
+;; (add-hook 'after-make-frame-functions
+;;           ( lambda (f)
+;;             (+load-face-font f)
+;;             (+load-ext-font)
+;;             (+load-emoji-font)))
 
-(+load-font)
+;; (+load-font)
 
 ;; all-the-icons 和 fire-code-mode 只能在 GUI 模式下使用。
 (when (display-graphic-p)
